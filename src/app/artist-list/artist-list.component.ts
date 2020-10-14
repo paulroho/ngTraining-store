85c51d2edcc7b36fd9c7c768b0eea70f361f8109
Paul Rohorzka <paul.rohorzka@squer.at>
2020-10-14 11:34:49 +0200

    Bring artist list to life

--- src/app/artist-list/artist-list.component.ts
+++ src/app/artist-list/artist-list.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import Store from '../store/store';
 
 @Component({
   selector: 'app-artist-list',
   template: `
     <h2>Artists</h2>
     <div class="artist-list">
-      <div class="item" *ngFor="let artist of artists">
+      <div class="item" *ngFor="let artist of artists$ | async">
         <b>{{ artist }}</b>
       </div>
     </div>
@@ -13,5 +16,10 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class ArtistListComponent {
-  public artists = ['Nirvana', 'Johnny Cash', 'Queen'];
+  public artists$ : Observable<string[]>;
+
+  constructor(private store: Store) {
+    const tracks$ = this.store.select(state => state.tracks);
+    this.artists$ = tracks$.pipe(map(tracks => [...new Set(tracks.map(track => track.artist))]));
+  }
 }
