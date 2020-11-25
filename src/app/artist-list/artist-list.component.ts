8250affb72aa4df543912b449c58c0629f2602c1
Paul Rohorzka <paul.rohorzka@squer.at>
2020-11-25 16:36:12 +0100

    Provide observable artists

--- src/app/artist-list/artist-list.component.ts
+++ src/app/artist-list/artist-list.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Artist } from '../artist';
 
 @Component({
   selector: 'app-artist-list',
   template: `
     <h2>Artists</h2>
     <div class="artist-list">
-      <div class="item" *ngFor="let artist of artists">
-        <b>{{ artist }}</b>
+      <div class="item" *ngFor="let artist of artists$ | async">
+        <b>{{ artist.name }}</b>
       </div>
     </div>
   `,
   styles: []
 })
 export class ArtistListComponent {
-  public artists = ['Nirvana', 'Johnny Cash', 'Queen'];
+  public artists$: Observable<Artist[]> = of([{name:'Nirvana'}, {name:'Johnny Cash'}, {name: 'Queen'}]);
 }
